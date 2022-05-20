74dbc6f5625a1cc76021eb943561f1464a5241db
tecnicos ti <ti@elretiro.gov.co>
2022-05-20 17:32:36 -0500

    commit 20/05/2022

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = ('/', (req = request, res = response) => {
     const {q, name = 'No name', apikey} = req.query;
@@ -13,9 +15,27 @@ const usuariosGet = ('/', (req = request, res = response) => {
     });
 })
 const usuariosPost = ('/', async (req = request, res = response) => {
-    const body = req.body;
-    const usuario = new Usuario( body );
+    const errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+    }
+
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario( {nombre, correo, password, rol} );
+
+    //Verificar correo
+    const existeEmail = await Usuario.findOne({ correo});
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'Ya existe una cuenta con ese correo electrónico'
+        })
+    }
+    // encriptar pass
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    //guardar DB
     await usuario.save();
     res.json({
         usuario
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const { usuariosGet, 
         usuariosPost, 
@@ -10,7 +11,12 @@ const router = Router();
 
 router.get('/', usuariosGet)
 
-router.post('/', usuariosPost)
+router.post('/', [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password es obligatorio y de mas de 6 caracteres').isLength({ min: 6 }),
+        check('correo', 'El correo no es válido').isEmail(),
+        check('rol', 'No es un rol valido').isIn(['ADMIN_ROL', 'USER_ROL']),
+], usuariosPost)
 
 router.put('/:id', usuariosPut)
 
